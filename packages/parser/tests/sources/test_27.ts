9d8e75360f9207ac4884f5e4ccb395144be67e9b
“chenhuachun” <samchen08@163.com>
2025-08-22 08:52:39 +0800

    fix: 🐛 修复模版表达式解析json丢失libs问题

--- packages/parser/tests/sources/test_27.ts
+++ packages/parser/tests/sources/test_27.ts
@@ -1,26 +1,67 @@
 export const test_27 = `
 <template>
-<div class="login-form">
-  <span v-if="false">Text</span>
-  <input v-else v-model="state.value"  />
- </div>
+   <div class="collapse-btn" @click="toggleCollapse">
+        <XIcon
+          :icon="state.isCollapse ? ArrowRightBold : ArrowLeftBold"
+          :size="18"></XIcon>
+      </div>
 </template>
 <script lang="ts">
   // @ts-nocheck
   import { defineComponent, reactive } from 'vue';
+  import {
+    ElDrawer,
+    ElMenu,
+    ElMenuItem,
+    ElSubMenu,
+    ElTooltip
+  } from 'element-plus';
+  import { XIcon } from '@vtj/ui';
+  import { ArrowRightBold, ArrowLeftBold } from '@vtj/icons';
   import { useProvider } from '@vtj/renderer';
   export default defineComponent({
-    name: 'Abc1',
-    components: {  },
+    name: '',
+    components: { ElDrawer, ElMenu, ElMenuItem, ElSubMenu, XIcon, ElTooltip },
+    props: {
+      menuData: {
+        type: [Array],
+        required: true,
+        default: () => []
+      },
+      isCollapse: {
+        type: [Boolean],
+        required: false,
+        default: false
+      },
+      activeMenu: {
+        type: [String],
+        required: true,
+        default: ''
+      },
+      isMobile: {
+        type: [Boolean],
+        required: false,
+        default: false
+      },
+      sidebarVisible: {
+        type: [Boolean],
+        required: false,
+        default: false
+      }
+    },
+    emits: ['toggle-collapse', 'select', 'update:sidebarVisible'],
     setup(props) {
-      const provider = useProvider({ id: '17zuumip', version: '1753066495241' });
+      const provider = useProvider({ id: '998tws2i', version: '1755786564748' });
       const state = reactive({
-      value:'abc'
+        isCollapse: props.isCollapse
       });
-      return { state, props, provider,mock };
+      return { state, props, provider, ArrowRightBold, ArrowLeftBold };
     }
   })
 </script>
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+
+</style>
+
 
 `;
