c17daabe6840fe005b87b9571d095d53785bf3c5
“chenhuachun” <samchen08@163.com>
2026-06-01 20:31:30 +0800

    fix: 🐛 在 mockApi 中添加 method 参数以支持不同请求方法

--- packages/renderer/src/provider/apis.ts
+++ packages/renderer/src/provider/apis.ts
@@ -138,31 +138,35 @@ export interface MockCallbackOptions {
 
 export function mockApi(Mock: any, schema: ApiSchema) {
   if (!schema.mock) return;
-  const { url, mockTemplate } = schema;
+  const { url, mockTemplate, method = 'get' } = schema;
   if (url && mockTemplate) {
     try {
       const path = isUrl(url) ? new URL(url).pathname : url;
       const regexp = pathToRegexp(`(.*)${path}(.*)`);
       const match = pathToRegexpMatch(path, { decode: decodeURIComponent });
       const handler = parseExpression(mockTemplate, {}, true);
-      Mock.mock(regexp, (options: MockCallbackOptions) => {
-        const query = urlUtil.parse(options.url) || {};
-        const data =
-          options.body instanceof FormData
-            ? formDataToJson(options.body)
-            : options.body;
-        const oPath = isUrl(options.url)
-          ? new URL(options.url).pathname
-          : options.url.split('?')[0];
-        const params = (match(oPath) as any)?.params;
-        Object.assign(options, { data, params, query });
-        try {
-          return Mock.mock(handler(options));
-        } catch (e) {
-          console.warn('[mockApi]', regexp, options, e);
-          return null;
+      Mock.mock(
+        regexp,
+        method.toLowerCase(),
+        (options: MockCallbackOptions) => {
+          const query = urlUtil.parse(options.url) || {};
+          const data =
+            options.body instanceof FormData
+              ? formDataToJson(options.body)
+              : options.body;
+          const oPath = isUrl(options.url)
+            ? new URL(options.url).pathname
+            : options.url.split('?')[0];
+          const params = (match(oPath) as any)?.params;
+          Object.assign(options, { data, params, query });
+          try {
+            return Mock.mock(handler(options));
+          } catch (e) {
+            console.warn('[mockApi]', regexp, options, e);
+            return null;
+          }
         }
-      });
+      );
     } catch (e) {
       console.warn('mockApi', e);
     }
