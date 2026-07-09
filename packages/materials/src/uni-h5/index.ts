4e3339539def65e1fdc90b2fe6e61ead9d2ce1d1
“chenhuachun” <samchen08@163.com>
2026-07-09 14:58:23 +0800

    feat(uni-h5): 注入全局Uni对象并优化安装逻辑
    - 增加injectUniGlobal函数，将UniH5相关对象注入指定全局变量
    - 在install函数中同时注入top和window全局对象，增强兼容性
    - 统一导出@dcloudio/uni-h5模块所有内容
    - 删除原本手动赋值top.uni和getApp的代码，改用注入函数实现

--- packages/materials/src/uni-h5/index.ts
+++ packages/materials/src/uni-h5/index.ts
@@ -3,12 +3,36 @@ import './style.scss';
 import type { App } from 'vue';
 //@ts-ignore
 import { plugin, getApp, uni } from '@dcloudio/uni-h5';
+
+//@ts-ignore
+import * as UniH5 from '@dcloudio/uni-h5';
+
 //@ts-ignore
 export * from '@dcloudio/uni-h5';
+
+export function injectUniGlobal(UniH5: any, global: any = window) {
+  const {
+    UniServiceJSBridge,
+    UniViewJSBridge,
+    getApp,
+    uni,
+    getCurrentPages,
+    upx2px,
+    setupPage
+  } = UniH5;
+  global.UniServiceJSBridge = UniServiceJSBridge;
+  global.UniViewJSBridge = UniViewJSBridge;
+  global.getApp = getApp;
+  global.uni = uni;
+  global.wx = uni;
+  global.getCurrentPages = getCurrentPages;
+  global.upx2px = upx2px;
+  global.__setupPage = (comp: any) => setupPage(comp);
+}
 
 export function install(app: App) {
   app.use(plugin);
   const top: any = window.top || window;
-  top.uni = (window as any).uni = uni;
-  top.getApp = (window as any).getApp = getApp;
+  injectUniGlobal(UniH5, top);
+  injectUniGlobal(UniH5, window);
 }
